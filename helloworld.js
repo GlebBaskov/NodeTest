ce4984e54360dd7d3feeaa7dc801a11d1b13216f
Gleb <glebbaskov@yandex.ru>
2025-09-04 20:38:57 +0300

    Initial

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/text' })
+    res.end('Server is running on port ' + port)
+})
+server.listen(port, () => console.log(`сервер запущен на порте ${port}; ` + 'нажмите Ctrl+C для завершения...'))
